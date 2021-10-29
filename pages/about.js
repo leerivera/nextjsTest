49416c101055db926f19208e9e5b766013b0d0be
leerivera <leerivera80@icloud.com>
2021-10-29 14:11:48 -0400

    cards component

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About  = () => {
+    return (  
+        <div>
+            <h1>Details about app</h1>
+        </div>
+    );
+}
+ 
+export default About;
---
+++ pages/cards/index.js
@@ -0,0 +1,9 @@
+const Cards = () => {
+    return (  
+        <div>
+            all cards
+        </div>
+    );
+}
+ 
+export default Cards;

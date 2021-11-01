08e1bc2e5e2abae4cf98c8802a034f9650237448
leerivera <leerivera80@icloud.com>
2021-11-01 12:57:37 -0400

    navbar

---
+++ comps/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className ="logo">
+                <h1>The List</h1>
+
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Cards</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>HomePage</h1>
     
     </div>
